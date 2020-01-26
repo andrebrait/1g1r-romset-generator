bc787bc47938e631f5518b1cf2c380ceb7ef1402
Andre Brait <andrebrait@gmail.com>
2020-01-26 20:14:00 +0100

    Add support for versions and revisions in any format, add verbose mode, add option for the DAT file

--- generate.py
+++ generate.py
@@ -6,7 +6,9 @@ from typing import Optional, Match, List, Dict
 
 import datafile
 
-UNSELECTED_REGION = 9999
+NOT_PRERELEASE = "ZZZZZZZZZZ"
+UNSELECTED_REGION = 1000
+BLACKLISTED_ROM_BASE = 500
 country_region_translation = [
     ('Australia', 'AUS'),
     ('Brazil', 'BRA'),
@@ -44,12 +46,12 @@ class GameEntry:
             is_bad: bool,
             region_index: int,
             input_index: int,
-            revision: int,
-            version: float,
-            sample: int,
-            demo: int,
-            beta: int,
-            proto: int,
+            revision: str,
+            version: str,
+            sample: str,
+            demo: str,
+            beta: str,
+            proto: str,
             rom: datafile.rom):
         self.is_bad = is_bad
         self.region_index = region_index
@@ -63,35 +65,30 @@ class GameEntry:
         self.rom = rom
 
 
-beta_regex = re.compile('\\(Beta(?:\\s*([0-9]+|[a-z]))?\\)', re.IGNORECASE)
-proto_regex = re.compile('\\(Proto(?:\\s*([0-9]+|[a-z]))?\\)', re.IGNORECASE)
-sample_regex = re.compile('\\(Sample(?:\\s*([0-9]+|[a-z]))?\\)', re.IGNORECASE)
-demo_regex = re.compile('\\(Demo(?:\\s*([0-9]+|[a-z]))?\\)', re.IGNORECASE)
-rev_regex = re.compile('\\(Rev\\s*([0-9]+|[a-z])\\)', re.IGNORECASE)
-version_regex = re.compile('\\(v\\s*([0-9]+(?:\\.[0-9]+))?\\)', re.IGNORECASE)
+beta_regex = re.compile('\\(Beta(?:\\s*([a-z0-9.]+))?\\)', re.IGNORECASE)
+proto_regex = re.compile('\\(Proto(?:\\s*([a-z0-9.]+))?\\)', re.IGNORECASE)
+sample_regex = re.compile('\\(Sample(?:\\s*([a-z0-9.]+))?\\)', re.IGNORECASE)
+demo_regex = re.compile('\\(Demo(?:\\s*([a-z0-9.]+))?\\)', re.IGNORECASE)
+rev_regex = re.compile('\\(Rev\\s*([a-z0-9.]+)\\)', re.IGNORECASE)
+version_regex = re.compile('\\(v\\s*([a-z0-9.]+)?\\)', re.IGNORECASE)
 sections_regex = re.compile('\\(([^()]+)\\)')
 
 
-def parse_revision(name: str):
+def parse_revision(name: str) -> str:
     rev_matcher = rev_regex.search(name)
     if rev_matcher:
-        try:
-            return int(rev_matcher.group(1))
-        except ValueError:
-            return ord(rev_matcher.group(1).upper())
-    else:
-        return 0
+        return rev_matcher.group(1)
+    return '0'
 
 
-def parse_version(name: str):
+def parse_version(name: str) -> str:
     version_matcher = version_regex.search(name)
     if version_matcher:
-        return float(version_matcher.group(1))
-    else:
-        return 0
+        return version_matcher.group(1)
+    return '0'
 
 
-def parse_regions(name: str):
+def parse_regions(name: str) -> List[str]:
     parsed = []
     for section in sections_regex.finditer(name):
         elements = [element.strip() for element in section.group(1).split(',')]
@@ -102,18 +99,15 @@ def parse_regions(name: str):
     return parsed
 
 
-def get_region_index(region: str, selected_regions: List[str]):
+def get_region_index(region: str, selected_regions: List[str]) -> int:
     try:
         return selected_regions.index(region)
     except ValueError:
         return UNSELECTED_REGION
 
 
-def parse_prerelease(match: Optional[Match]):
-    try:
-        return int(match.group(1)) if match else 9999
-    except ValueError:
-        return ord(match.group(1).upper()) if match else 9999
+def parse_prerelease(match: Optional[Match]) -> str:
+    return match.group(1) if match else NOT_PRERELEASE
 
 
 def parse_games(
@@ -125,7 +119,9 @@ def parse_games(
         filter_demo: bool,
         filter_sample: bool,
         filter_proto: bool,
-        all_regions: bool) -> Dict[str, List[GameEntry]]:
+        all_regions: bool,
+        blacklist: List[str],
+        verbose: bool) -> Dict[str, List[GameEntry]]:
     games = {}
     root = datafile.parse(file, silence=True)
     for input_index in range(0, len(root.game)):
@@ -157,15 +153,26 @@ def parse_games(
         revision = parse_revision(game.name)
         version = parse_version(game.name)
         region_indexes = []
-        if game.release:
-            for release in game.release:
-                region_indexes.append(get_region_index(release.region, selected_regions))
-        else:
-            region_indexes.append(UNSELECTED_REGION)
+        for release in game.release:
+            region_index = get_region_index(release.region, selected_regions)
+            if region_index not in region_indexes:
+                region_indexes.append(region_index)
         for region in parse_regions(game.name):
             region_index = get_region_index(region, selected_regions)
             if region_index not in region_indexes:
                 region_indexes.append(region_index)
+        if not region_indexes:
+            region_indexes.append(UNSELECTED_REGION)
+        if blacklist:
+            for string in blacklist:
+                if string in game.name:
+                    if verbose:
+                        print(
+                            'Penalizing candidate [' + game.name + ']. ' +
+                            'Reason: contains blacklisted string [' + string + ']',
+                            file=sys.stderr)
+                    region_indexes = [BLACKLISTED_ROM_BASE + x for x in region_indexes]
+                    break
         for rom in game.rom:
             for region_index in region_indexes:
                 if all_regions or region_index < UNSELECTED_REGION:
@@ -184,10 +191,18 @@ def parse_games(
     return games
 
 
+def replace_extension(file_extension, file_name):
+    try:
+        return file_name[:file_name.rindex(os.extsep)] + os.extsep + file_extension
+    except ValueError:
+        return file_name + os.extsep + file_extension
+
+
 def main(argv: List[str]):
     try:
-        opts, args = getopt.getopt(argv, 'hr:e:i:', [
+        opts, args = getopt.getopt(argv, 'hd:r:e:i:b:v', [
             'help',
+            'dat=',
             'regions=',
             'no-bios',
             'no-unlicensed',
@@ -201,13 +216,16 @@ def main(argv: List[str]):
             'early-versions',
             'input-order',
             'extension=',
-            'input-dir='
+            'input-dir=',
+            'blacklist=',
+            'verbose'
         ])
     except getopt.GetoptError as e:
         print(e, file=sys.stderr)
         print_help()
         sys.exit(2)
 
+    dat_file = None
     filter_bios = False
     filter_unlicensed = False
     filter_beta = False
@@ -215,12 +233,14 @@ def main(argv: List[str]):
     filter_sample = False
     filter_proto = False
     all_regions = False
-    rev_multiplier = -1
-    version_multiplier = -1
+    revision_asc = False
+    version_asc = False
+    verbose = False
     index_multiplier = 0
     selected_regions = None
     file_extension = None
     input_dir = None
+    blacklist = None
     for opt, arg in opts:
         if opt in ('-h', '--help'):
             print_help()
@@ -234,31 +254,42 @@ def main(argv: List[str]):
         filter_sample |= opt == '--no-sample' or opt == '--no-all'
         filter_proto |= opt == '--no-proto' or opt == '--no-all'
         all_regions |= opt == '--all-regions'
-        if opt == '--early-revisions':
-            rev_multiplier = 1
-        if opt == '--early-versions':
-            version_multiplier = 1
+        revision_asc |= opt == '--early-revisions'
+        version_asc |= opt == '--early-versions'
+        verbose |= opt in ('-v', '--verbose')
+        if opt in ('-d', '--dat'):
+            dat_file = os.path.expanduser(arg)
+            if not os.path.isfile(dat_file):
+                print('invalid DAT file: ' + dat_file, file=sys.stderr)
+                print_help()
+                sys.exit(2)
         if opt in ('-e', '--extension'):
             file_extension = arg.lstrip(os.extsep)
         if opt == '--input-order':
             index_multiplier = 1
+        if opt in ('-b', '--blacklist'):
+            blacklist = arg.split(',')
         if opt in ('-i', '--input-dir'):
-            input_dir = arg.rstrip(os.sep)
+            input_dir = os.path.expanduser(arg)
             if not os.path.isdir(input_dir):
-                print('invalid input directory: ' + input_dir)
+                print('invalid input directory: ' + input_dir, file=sys.stderr)
                 print_help()
                 sys.exit(2)
+    if not dat_file:
+        print('DAT file is required', file=sys.stderr)
+        print_help()
+        sys.exit(2)
     if not selected_regions:
         print('invalid region selection', file=sys.stderr)
         print_help()
         sys.exit(2)
-    if (rev_multiplier > 0 or version_multiplier > 0) and index_multiplier > 0:
+    if (revision_asc or version_asc) and index_multiplier > 0:
         print('early-revisions and early-versions are mutually exclusive with input-order', file=sys.stderr)
         print_help()
         sys.exit(2)
 
     games = parse_games(
-        args[0],
+        dat_file,
         selected_regions,
         filter_bios,
         filter_unlicensed,
@@ -266,33 +297,37 @@ def main(argv: List[str]):
         filter_demo,
         filter_sample,
         filter_proto,
-        all_regions)
+        all_regions,
+        blacklist,
+        verbose)
 
-    for game in games:
-        games[game].sort(key=lambda x: (
-            x.is_bad,
-            x.region_index,
-            index_multiplier * x.input_index,
-            rev_multiplier * x.revision,
-            version_multiplier * x.version,
-            -x.sample,
-            -x.demo,
-            -x.beta,
-            -x.proto,
-            len(x.rom.name)))
+    for key in games:
+        game_entries = games[key]
+        game_entries.sort(key=lambda x: len(x.rom.name))
+        game_entries.sort(key=lambda x: (x.sample, x.demo, x.beta, x.proto), reverse=True)
+        game_entries.sort(key=lambda x: x.version, reverse=not version_asc)
+        game_entries.sort(key=lambda x: x.revision, reverse=not revision_asc)
+        game_entries.sort(key=lambda x: (x.is_bad, x.region_index, index_multiplier * x.input_index))
+        if verbose:
+            print('Candidate order for [' + key + ']: ' + str([x.rom.name for x in game_entries]), file=sys.stderr)
 
     for game, entries in games.items():
         for entry in entries:
             file_name = entry.rom.name
             if file_extension:
-                file_name = file_name[:file_name.rindex(os.extsep)] + os.extsep + file_extension
+                file_name = replace_extension(file_extension, file_name)
             if input_dir:
-                full_path = input_dir + os.sep + file_name
+                full_path = os.path.join(input_dir, file_name)
                 if os.path.isfile(full_path):
                     print(file_name)
                     break
                 else:
-                    print("WARNING: file [" + full_path + "] not found, attempting next candidate", file=sys.stderr)
+                    if verbose:
+                        print(
+                            "WARNING [" + game + "]: candidate [" + file_name + "] not found, trying next one",
+                            file=sys.stderr)
+                    else:
+                        print("WARNING: candidate [" + file_name + "] not found, trying next one", file=sys.stderr)
             else:
                 print(file_name)
                 break
@@ -303,6 +338,7 @@ def print_help():
     print('Options:', file=sys.stderr)
     print('\t-h,--help\tPrints this usage message', file=sys.stderr)
     print('\t-r,--regions=REGIONS\tA list of regions separated by commas. Ex.: -r USA,EUR,JPN', file=sys.stderr)
+    print('\t-d,--dat=DAT_FILE\tThe DAT file to be used', file=sys.stderr)
     print('\t--no-bios\tFilter out BIOSes', file=sys.stderr)
     print('\t--no-unlicensed\tFilter out unlicensed ROMs', file=sys.stderr)
     print('\t--no-beta\tFilter out beta ROMs', file=sys.stderr)
@@ -315,6 +351,10 @@ def print_help():
     print('\t--early-versions\tROMs of earlier versions will be prioritized', file=sys.stderr)
     print('\t--input-order\tROMs will be prioritized by the order they appear in the DAT file', file=sys.stderr)
     print('\t-e,--extension=EXTENSION\tROM names will use this extension. Ex.: -e zip', file=sys.stderr)
+    print(
+        '\t-b,--blacklist=WORDS\tROMs containing these strings will be avoided. Ex.: -b "Virtual Console,GameCube"',
+        file=sys.stderr)
+    print('\t-v,--verbose\tPrints more messages (useful when troubleshooting)',file=sys.stderr)
     print('\t-i,--input-dir=PATH\tProvides an input directory (i.e.: where your ROMs are)', file=sys.stderr)
 
 
