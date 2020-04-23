107f47ea83b3e6fa035fd75814592364a45f9856
Andre Brait <andrebrait@gmail.com>
2020-04-23 11:20:04 +0200

    Refactoring, check if file is in context before using it

--- modules/archive_support.py
+++ modules/archive_support.py
@@ -4,9 +4,7 @@ import tempfile
 import zipfile
 from abc import ABC, abstractmethod
 from contextlib import AbstractContextManager
-from tarfile import TarFile, TarInfo
 from typing import List, IO, Optional, Dict, AnyStr
-from zipfile import ZipFile, ZipInfo
 
 TAR_KEY = 'tar'
 ZIP_KEY = 'zip'
@@ -16,21 +14,28 @@ SEVENZIP_KEY = '7z'
 _SUPPORTED_ARCHIVE_TYPES: Dict[str, 'ArchiveType'] = {}
 
 
-def get_archive_type(path: str) -> Optional['ArchiveType']:
+def for_file(path: str) -> Optional['ArchiveType']:
     for t in _SUPPORTED_ARCHIVE_TYPES.values():
-        if t.check_type(path):
+        if t.supports(path):
             return t
     return None
+
+
+def by_key(key: str) -> Optional['ArchiveType']:
+    if key in _SUPPORTED_ARCHIVE_TYPES:
+        return _SUPPORTED_ARCHIVE_TYPES[key]
+    else:
+        return None
 
 
 class ArchiveType(ABC):
 
     @abstractmethod
-    def check_type(self, path: str) -> bool:
+    def supports(self, path: str) -> bool:
         pass
 
     @abstractmethod
-    def create_object(self, path: str, mode: str = 'r') -> 'Archive':
+    def open(self, path: str, mode: str = 'r') -> 'Archive':
         pass
 
 
@@ -39,12 +44,16 @@ class Archive(AbstractContextManager):
             self,
             full_path: str,
             mode: str):
+        if mode not in ("r", "w"):
+            raise ValueError('open() requires mode "r" or "w"')
         self.__full_path = full_path
         self.__mode = mode
 
+    @property
     def full_path(self) -> str:
         return self.__full_path
 
+    @property
     def mode(self) -> str:
         return self.__mode
 
@@ -68,6 +77,11 @@ class Archive(AbstractContextManager):
     def __exit__(self, typ, value, traceback):
         pass
 
+    @staticmethod
+    def _check_context(archive_file):
+        if not archive_file:
+            raise ValueError('Attempt to use an archive out of context')
+
 
 class _ZipArchive(Archive):
 
@@ -76,34 +90,37 @@ class _ZipArchive(Archive):
             full_path: str,
             mode: str = 'r'):
         super().__init__(full_path, mode)
-        self.__archive: Optional[ZipFile] = None
+        self.__archive: Optional[zipfile.ZipFile] = None
 
     def file_names(self) -> List[str]:
-        infos: List[ZipInfo] = self.__archive.infolist()
+        Archive._check_context(self.__archive)
+        infos: List[zipfile.ZipInfo] = self.__archive.infolist()
         return [i.filename for i in infos if not i.is_dir()]
 
     def size(self, file_name: str) -> int:
-        info: ZipInfo = self.__archive.getinfo(file_name)
+        Archive._check_context(self.__archive)
+        info: zipfile.ZipInfo = self.__archive.getinfo(file_name)
         return info.file_size
 
     def open(self, file_name: str) -> IO[AnyStr]:
-        return self.__archive.open(file_name, self.mode())
+        Archive._check_context(self.__archive)
+        return self.__archive.open(file_name, self.mode)
 
     def __enter__(self) -> 'Archive':
-        self.__archive = ZipFile(self.full_path(), self.mode())
+        self.__archive = zipfile.ZipFile(self.full_path, self.mode).__enter__()
         return self
 
     def __exit__(self, typ, value, traceback):
-        self.__archive.close()
+        self.__archive.__exit__(typ, value, traceback)
         self.__archive = None
 
 
 class _ZipArchiveType(ArchiveType):
 
-    def check_type(self, path: str) -> bool:
+    def supports(self, path: str) -> bool:
         return zipfile.is_zipfile(path)
 
-    def create_object(self, path: str, mode: str = 'r') -> 'Archive':
+    def open(self, path: str, mode: str = 'r') -> 'Archive':
         return _ZipArchive(path, mode)
 
 
@@ -117,34 +134,37 @@ class _TarArchive(Archive):
             full_path: str,
             mode: str = 'r'):
         super().__init__(full_path, mode)
-        self.__archive: Optional[TarFile] = None
+        self.__archive: Optional[tarfile.TarFile] = None
 
     def file_names(self) -> List[str]:
-        infos: List[TarInfo] = self.__archive.getmembers()
+        Archive._check_context(self.__archive)
+        infos: List[tarfile.TarInfo] = self.__archive.getmembers()
         return [i.name for i in infos if i.isfile()]
 
     def size(self, file_name: str) -> int:
-        info: TarInfo = self.__archive.getmember(file_name)
+        Archive._check_context(self.__archive)
+        info: tarfile.TarInfo = self.__archive.getmember(file_name)
         return info.size
 
     def open(self, file_name: str) -> IO[AnyStr]:
-        return self.__archive.open(file_name, self.mode())
+        Archive._check_context(self.__archive)
+        return self.__archive.open(file_name, self.mode)
 
     def __enter__(self) -> 'Archive':
-        self.__archive = TarFile(self.full_path(), self.mode())
+        self.__archive = tarfile.TarFile(self.full_path, self.mode).__enter__()
         return self
 
     def __exit__(self, typ, value, traceback):
-        self.__archive.close()
+        self.__archive.__exit__(typ, value, traceback)
         self.__archive = None
 
 
 class _TarArchiveType(ArchiveType):
 
-    def check_type(self, path: str) -> bool:
+    def supports(self, path: str) -> bool:
         return tarfile.is_tarfile(path)
 
-    def create_object(self, path: str, mode: str = 'r') -> 'Archive':
+    def open(self, path: str, mode: str = 'r') -> 'Archive':
         return _TarArchive(path, mode)
 
 
@@ -152,68 +172,70 @@ _SUPPORTED_ARCHIVE_TYPES[TAR_KEY] = _TarArchiveType()
 
 try:
     import rarfile
-    from rarfile import RarFile
-    from rarfile import RarInfo
-
 
     class _RarArchive(Archive):
 
         def __init__(self, full_path: str, mode: str):
             super().__init__(full_path, mode)
-            self.__archive: Optional[RarFile] = None
+            self.__archive: Optional[rarfile.RarFile] = None
 
         def file_names(self) -> List[str]:
-            infos: List[RarInfo] = self.__archive.infolist()
+            Archive._check_context(self.__archive)
+            infos: List[rarfile.RarInfo] = self.__archive.infolist()
             return [i.filename for i in infos if not i.isdir()]
 
         def size(self, file_name: str) -> int:
-            info: RarInfo = self.__archive.getinfo(file_name)
+            Archive._check_context(self.__archive)
+            info: rarfile.RarInfo = self.__archive.getinfo(file_name)
             return info.file_size
 
         def open(self, file_name: str) -> IO[AnyStr]:
-            return self.__archive.open(file_name, self.mode())
+            Archive._check_context(self.__archive)
+            return self.__archive.open(file_name, self.mode)
 
         def __enter__(self) -> 'Archive':
-            self.__archive = RarFile(self.full_path(), self.mode())
+            self.__archive = \
+                rarfile.RarFile(self.full_path, self.mode).__enter__()
             return self
 
         def __exit__(self, typ, value, traceback):
-            self.__archive.close()
+            self.__archive.__exit__(typ, value, traceback)
+            self.__archive = None
 
 
     class _RarArchiveType(ArchiveType):
-        def check_type(self, path: str) -> bool:
+
+        def supports(self, path: str) -> bool:
             return rarfile.is_rarfile(path)
 
-        def create_object(self, path: str, mode: str = 'r') -> 'Archive':
+        def open(self, path: str, mode: str = 'r') -> 'Archive':
             return _RarArchive(path, mode)
 
 
     _SUPPORTED_ARCHIVE_TYPES[RAR_KEY] = _RarArchiveType()
 
-except ImportError:
+except ImportError as e:
+    rarfile = None
     print(
-        "WARNING: Support for RAR archives requires the Python module "
-        "'rarfile' to be installed",
+        "WARNING: Disabling support for RAR archives: %s" % e,
         file=sys.stderr)
 
 try:
     import py7zr
-    from py7zr import SevenZipFile
     import os
     import shutil
-    from contextlib import AbstractContextManager
 
 
     class _SevenZipArchive(Archive):
 
         def __init__(self, full_path: str, mode: str):
             super().__init__(full_path, mode)
-            self.__archive: Optional[SevenZipFile] = None
+            self.__archive: Optional[py7zr.SevenZipFile] = None
             self.__temp_dir: Optional[str] = None
             self.__extracted = False
 
         def file_names(self) -> List[str]:
+            Archive._check_context(self.__archive)
             return [i.filename for i in self.__archive.files if
                     not i.is_directory and
                     not i.is_junction and
@@ -221,12 +243,16 @@ try:
                     not i.is_symlink]
 
         def size(self, file_name: str) -> int:
-            for i in self.__archive.list():
+            Archive._check_context(self.__archive)
+            for i in self.__archive.files:
                 if i.filename == file_name:
                     return i.uncompressed
+            raise KeyError(
+                'There is no item named %s in the archive' % file_name)
 
         def open(self, file_name: str) -> IO[AnyStr]:
-            if self.mode() == 'w':
+            Archive._check_context(self.__archive)
+            if 'w' in self.mode:
                 if not self.__temp_dir:
                     self.__temp_dir = tempfile.mkdtemp()
                 full_path = os.path.join(self.__temp_dir, file_name)
@@ -242,15 +268,16 @@ try:
                 return open(full_path, 'rb')
 
         def __enter__(self) -> 'Archive':
-            self.__archive = SevenZipFile(self.full_path(), self.mode())
+            self.__archive = \
+                py7zr.SevenZipFile(self.full_path, self.mode).__enter__()
             return self
 
         def __exit__(self, typ, value, traceback):
             if self.__temp_dir:
-                if self.mode() == 'w':
+                if 'w' in self.mode:
                     self.__archive.writeall(self.__temp_dir, '/')
                 self.__archive.__exit__(typ, value, traceback)
-                shutil.rmtree(self.__temp_dir)
+                shutil.rmtree(self.__temp_dir, ignore_errors=True)
             else:
                 self.__archive.__exit__(typ, value, traceback)
             self.__temp_dir = None
@@ -258,17 +285,18 @@ try:
 
 
     class _SevenZipArchiveType(ArchiveType):
-        def check_type(self, path: str) -> bool:
+
+        def supports(self, path: str) -> bool:
             return py7zr.is_7zfile(path)
 
-        def create_object(self, path: str, mode: str = 'r') -> 'Archive':
+        def open(self, path: str, mode: str = 'r') -> 'Archive':
             return _SevenZipArchive(path, mode)
 
 
     _SUPPORTED_ARCHIVE_TYPES[SEVENZIP_KEY] = _SevenZipArchiveType()
 
-except ImportError:
+except ImportError as e:
+    py7zr = None
     print(
-        "WARNING: Support for 7Z archives requires the Python module "
-        "'py7zr' to be installed",
+        "WARNING: Disabling support for 7z archives: %s" % e,
         file=sys.stderr)
